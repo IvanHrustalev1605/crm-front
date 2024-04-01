047e862a58888acf29dab4208eac9433833a0cab
khrustalev <khrustalev16@bk.ru>
2024-04-01 22:51:23 +0300

    init1

---
+++ src/main/kotlin/com/example/crmfront/CrmFrontApplication.kt
@@ -0,0 +1,11 @@
+package com.example.crmfront
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrmFrontApplication
+
+fun main(args: Array<String>) {
+    runApplication<CrmFrontApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/crmfront/CrmFrontApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.crmfront
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CrmFrontApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
